faaa84b1ad5e17227d1295428e9e5aa44bfe53fd
David <zwssd1980@163.com>
2018-06-22 17:26:18 +0800

    已经可以下字符雨了，但获得键盘按键有问题，需要修改

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 extern "C" {
 #include <pty.h>
 #include <signal.h>
-#include<stdlib.h>  
+#include<stdlib.h>
 #include<unistd.h>
 #ifdef HAVE_NCURSES_H
 #include <ncurses.h>
@@ -42,70 +42,120 @@ void version(void);
 
 
 int main(int argc, char **argv) {
-    int update = 5, bold = -1, oldstyle = 0;
+    int update = 5, bold = -1, oldstyle = 0,
+        i=0, j=0, random = 0, randnum = 0, randmin = 0, highnum = 0, y, z;
     int optchr, keypress;
-    
+
     /* Many thanks to zwssd- (349705141@qq.com) for this getopt patch */
     opterr = 0;
     while ((optchr = getopt(argc, argv, "bBohV")) != EOF) {
-	switch (optchr) {
-	case 'b':
-	    if (bold != 2 && bold != 0)
-		bold = 1;
-	    break;
-	case 'B':
-	    if (bold != 0)
-		bold = 2;
-	    break;
-	case 'h':
-	case '?':
-	    usage();
-	    exit(0);
-	case 'o':
-	    oldstyle = 1;
-	    break;
-	case 'V':
-	    version();
-	    exit(0);
-	}
+        switch (optchr) {
+        case 'b':
+            if (bold != 2 && bold != 0)
+                bold = 1;
+            break;
+        case 'B':
+            if (bold != 0)
+                bold = 2;
+            break;
+        case 'h':
+        case '?':
+            usage();
+            exit(0);
+        case 'o':
+            oldstyle = 1;
+            break;
+        case 'V':
+            version();
+            exit(0);
+        }
     }
-    
+
     initscr();
     signal(SIGWINCH, sig_winch);
-    
-    /* Set up values for random number generation */
-    if (console || xwindow) {
-	randnum = 51;
-	randmin = 166;
-	highnum = 217;
-    } else {
-	randnum = 93;
+
+    randnum = 93;
 	randmin = 33;
 	highnum = 123;
-    }
-    
-    //std::cout << "Hello, world!" << std::endl;
-    
-    char a='a';
-    
-    /*while(1){
-        clear();
-        addch(a);
-        addch(' ');
-        addch(' ');
-        if(a=='a'){
-            a = 'z';
-        }else{
-            a = 'a';
+
+    matrix_init();
+    while(1) {
+        /*if ((keypress = wgetch(stdscr)) != ERR) {
+            switch (keypress) {
+            case 'q':
+                finish(0);
+                break;
+            case 'b':
+                bold = 1;
+                break;
+            case 'B':
+                bold = 2;
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                update = keypress - 48;
+                break;
+            }
+        }*/
+
+        for (j = 0; j <= COLS - 1; j += 2) {
+            for (i = LINES - 1; i >= 1; i--)
+                matrix[i][j].val = matrix[i - 1][j].val;
+
+            random = (int) rand() % (randnum + 8) + randmin;
+
+            if (matrix[1][j].val == 0)
+			matrix[0][j].val = 1;
+		    else if (matrix[1][j].val == ' '
+			     || matrix[1][j].val == -1) {
+			if (spaces[j] > 0) {
+			    matrix[0][j].val = ' ';
+			    spaces[j]--;
+			} else {
+			    if (((int) rand() % 3) == 1)
+				matrix[0][j].val = 0;
+			    else
+				matrix[0][j].val = (int) rand() % randnum
+				    + randmin;
+
+			    spaces[j] = (int) rand() % LINES + 1;
+			}
+		    } else if (random > highnum && matrix[1][j].val != 1)
+			matrix[0][j].val = ' ';
+		    else
+			matrix[0][j].val = (int) rand() % randnum + randmin;
+
+            y = 1;
+            z = LINES;
+            for (i = y; i <= z; i++) {
+                move(i - y, j);
+
+                if (matrix[i][j].val == 0) {
+                    addch('&');
+                } else
+                    addch(matrix[i][j].val);
+
+                if (matrix[i][j].val == -1)
+                    addch(' ');
+                else
+                    addch(matrix[i][j].val);
+            }
         }
-        addch(a);
-        
-        napms(update * 1);
+
         refresh();
-    }*/
-    
-    //endwin();
-    getchar();//等待按键
+        napms(update * 10);
+    }
+
+    endwin();
+    //getchar();//等待按键
     return 0;
 }
 
@@ -118,12 +168,12 @@ void sig_winch(int s)
 
     result = ioctl(fd, TIOCGWINSZ, &win);
     if (result == -1)
-	return;
-    
+        return;
+
     COLS = win.ws_col;
     LINES = win.ws_row;
-    
-    printf("%d rows, %d columns\n", win.ws_row, win.ws_col);//输出终端大小  
+
+    printf("%d rows, %d columns\n", win.ws_row, win.ws_col);//输出终端大小
 
     matrix_init();
     /* Do these b/c width may have changed... */
@@ -140,7 +190,7 @@ void *nmalloc(size_t howmuch)
     /* Panic save? */
 
     if (!(r = malloc(howmuch)))
-	c_die("ZMatrix: malloc: out of memory!");
+        c_die("ZMatrix: malloc: out of memory!");
 
     return r;
 }
@@ -151,41 +201,41 @@ void matrix_init(void)
     int i, j;
 
     if (matrix != NULL)
-	free(matrix);
+        free(matrix);
 
     matrix = (zmatrix **) nmalloc(sizeof(zmatrix) * (LINES + 1));
     for (i = 0; i <= LINES; i++)
-	matrix[i] = (zmatrix *) nmalloc(sizeof(zmatrix) * COLS);
+        matrix[i] = (zmatrix *) nmalloc(sizeof(zmatrix) * COLS);
 
     if (length != NULL)
-	free(length);
+        free(length);
     length = (int *) nmalloc(COLS * sizeof(int));
 
     if (spaces != NULL)
-	free(spaces);
+        free(spaces);
     spaces = (int *) nmalloc(COLS* sizeof(int));
 
     if (updates != NULL)
-	free(updates);
+        free(updates);
     updates = (int *) nmalloc(COLS * sizeof(int));
 
     /* Make the matrix */
     for (i = 0; i <= LINES; i++)
-	for (j = 0; j <= COLS - 1; j += 2)
-	    matrix[i][j].val = -1;
+        for (j = 0; j <= COLS - 1; j += 2)
+            matrix[i][j].val = -1;
 
     for (j = 0; j <= COLS - 1; j += 2) {
-	/* Set up spaces[] array of how many spaces to skip */
-	spaces[j] = (int) rand() % LINES + 1;
+        /* Set up spaces[] array of how many spaces to skip */
+        spaces[j] = (int) rand() % LINES + 1;
 
-	/* And length of the stream */
-	length[j] = (int) rand() % (LINES - 3) + 3;
+        /* And length of the stream */
+        length[j] = (int) rand() % (LINES - 3) + 3;
 
-	/* Sentinel value for creation of new objects */
-	matrix[1][j].val = ' ';
+        /* Sentinel value for creation of new objects */
+        matrix[1][j].val = ' ';
 
-	/* And set updates[] array for update speed. */
-	updates[j] = (int) rand() % 3 + 1;
+        /* And set updates[] array for update speed. */
+        updates[j] = (int) rand() % 3 + 1;
     }
 }
 
@@ -211,10 +261,10 @@ void finish(int sigage)
     endwin();
 #ifdef HAVE_CONSOLECHARS
     if (console)
-	va_system("consolechars -d");
+        va_system("consolechars -d");
 #elif defined(HAVE_SETFONT)
     if (console)
-	va_system("setfont");
+        va_system("setfont");
 #endif
     exit(0);
 }
@@ -231,10 +281,10 @@ void c_die(char *msg, ...)
     endwin();
 #ifdef HAVE_CONSOLECHARS
     if (console)
-	va_system("consolechars -d");
+        va_system("consolechars -d");
 #elif defined(HAVE_SETFONT)
     if (console)
-	va_system("setfont");
+        va_system("setfont");
 #endif
 
     va_start(ap, msg);
@@ -256,6 +306,6 @@ void usage(void)
 void version(void)
 {
     printf(" ZMatrix version %s by zwssd (compiled %s, %s)\n",
-	   "0.000001", __TIME__, __DATE__);
+           "0.000001", __TIME__, __DATE__);
     printf(" Email: 349705141@qq.com  Web: https://github.com/zwssd/zmatrix\n");
 }
